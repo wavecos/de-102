5e2fff4997b71ab6e10132d3a5674b5b5f2372da
Alfredo Arias <alfredo@xiobit.com>
2019-03-18 21:16:36 -0400

    rxjs

---
+++ petclinic/src/app/model/Cancion.ts
@@ -0,0 +1,11 @@
+export class Cancion { 
+    artista: string;
+    album: string;
+    cancion: string;
+    precio: number;
+    moneda: string;
+    fecha: string;
+    albumImagen: string;
+    vistaPrevia: string;
+    genero: string;
+}
--- petclinic/src/app/services/MusicService.ts
+++ petclinic/src/app/services/MusicService.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { Cancion } from '../model/Cancion';
+import { Observable } from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
@@ -8,8 +11,29 @@ export class MusicService {
 
     constructor(private http: HttpClient) {}
 
-    obtenerCanciones() {
-        return this.http.get("https://itunes.apple.com/search?term=beatles&entity=song&limit=10");
+    obtenerCanciones(): Observable<Cancion[]> {
+        return this.http.get("https://itunes.apple.com/search?term=beatles&entity=song&limit=10")
+                .pipe(map(jsonData => {
+                    var cancionesJson = jsonData['results'];
+                    var canciones: Cancion[] = [];
+
+                    cancionesJson.forEach(cancionJson => {
+                        var cancion = new Cancion();
+                        cancion.artista = cancionJson['artistName'];
+                        cancion.album = cancionJson['collectionName'];
+                        cancion.cancion = cancionJson['trackName'];
+                        cancion.fecha = cancionJson['releaseDate'];
+                        cancion.precio = cancionJson['trackPrice'];
+                        cancion.moneda = cancionJson['currency'];
+                        cancion.genero = cancionJson['primaryGenreName'];
+                        cancion.albumImagen = cancionJson['artworkUrl100'];
+                        cancion.vistaPrevia = cancionJson['previewUrl'];
+
+                        canciones.push(cancion);
+                    });
+                    
+                    return canciones;
+                }));
     }
 
 }
